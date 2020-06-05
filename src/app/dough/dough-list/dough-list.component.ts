a553b2e3eb171f9533ef7ca9ae8d0ac1a6dfb6d5
alannguyen5102 <alannguyen5102@gmail.com>
2020-06-04 19:31:48 -0700

    Adding Dough List

--- src/app/dough/dough-list/dough-list.component.ts
+++ src/app/dough/dough-list/dough-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../../model/ingredient';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dough-list',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dough-list.component.scss']
 })
 export class DoughListComponent implements OnInit {
+  
+  public ingredientList: Array<Ingredient>;
+  public percentageControl = new FormControl();
 
-  constructor() { }
-
+  constructor() { 
+  }
+  
   ngOnInit(): void {
+    this.ingredientList = [
+      new Ingredient("AP Flour", 800, "g"),
+      new Ingredient("Water", 600, "g"),
+      new Ingredient("Kosher Salt", 18, "g"),
+      new Ingredient("Yeast", 0.25, "tsp"),
+      new Ingredient("Honey", 15, "g"),
+      new Ingredient("Olive Oil", 50, "g")
+    ]
+
   }
 
 }
