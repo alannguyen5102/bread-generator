9bf39d05c177d217d42c0847df0998f05aad7758
alannguyen5102 <alannguyen5102@gmail.com>
2020-06-05 21:15:49 -0700

    added recipe class to dough list

--- src/app/dough/dough-list/dough-list.component.ts
+++ src/app/dough/dough-list/dough-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../../model/ingredient';
 import { FormControl } from '@angular/forms';
+import { Recipe } from 'src/app/model/recipe';
 
 @Component({
   selector: 'app-dough-list',
@@ -9,22 +10,22 @@ import { FormControl } from '@angular/forms';
 })
 export class DoughListComponent implements OnInit {
   
-  public ingredientList: Array<Ingredient>;
-  public percentageControl = new FormControl();
+  public ingredientList: Array<Ingredient> = [
+    new Ingredient("AP Flour", 800, "g"),
+    new Ingredient("Water", 600, "g"),
+    new Ingredient("Kosher Salt", 18, "g"),
+    new Ingredient("Yeast", 0.25, "tsp"),
+    new Ingredient("Honey", 15, "g"),
+    new Ingredient("Olive Oil", 50, "g")
+  ];
+  public recipe = new Recipe(this.ingredientList, "By Samin Nosrat");
+  public percentageControl = new FormControl(1);
 
   constructor() { 
   }
   
   ngOnInit(): void {
-    this.ingredientList = [
-      new Ingredient("AP Flour", 800, "g"),
-      new Ingredient("Water", 600, "g"),
-      new Ingredient("Kosher Salt", 18, "g"),
-      new Ingredient("Yeast", 0.25, "tsp"),
-      new Ingredient("Honey", 15, "g"),
-      new Ingredient("Olive Oil", 50, "g")
-    ]
-
+    console.log(this.ingredientList);
   }
 
 }
